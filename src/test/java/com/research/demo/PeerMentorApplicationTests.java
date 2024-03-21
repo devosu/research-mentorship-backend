654969078b480cf8dc3797a13acc4cc2c39e240e
Keming He <keminghe.career@gmail.com>
2024-03-21 13:17:54 -0400

    Initialized separate backend setup for DEV's Research Mentorship project.

---
+++ src/test/java/com/research/demo/PeerMentorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.research.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.dev.peerMentorResearch.PeerMentorApplication;
+
+@SpringBootTest(classes = PeerMentorApplication.class)
+class PeerMentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
