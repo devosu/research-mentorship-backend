e1b4c352bf3d76d6af883025dae3ea3af5e9a3d7
D'Andre Williams <williams.7638@osu.edu>
2024-03-27 12:56:47 -0400

    set up environment variable for mongoDB connection string to keep it secure

--- src/main/java/com/dev/peerMentorResearch/MongoDBConfig/MongoDbConfig.java
+++ src/main/java/com/dev/peerMentorResearch/MongoDBConfig/MongoDbConfig.java
@@ -3,6 +3,8 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.bson.Document;
@@ -11,26 +13,25 @@ import org.bson.Document;
 @Configuration
 public class MongoDbConfig {
 
+    //used environmental variable to store connection string
+     @Value("${MONGODB_CONNECTION_STRING}")
+    private String mongoConnectionString;
+
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb+srv://osupeermentorresearch:TTiRd1LRY42orLca@researchcluster.pfvocox.mongodb.net/");
+        return MongoClients.create(mongoConnectionString);
     }
     @Bean
     public MongoDatabase mongoDatabase() {
-        return mongoClient().getDatabase("researchDatabase");
+        return mongoClient().getDatabase("researchMentorshipData");
     }
     
     @Bean
     public MongoCollection<Document> mentorFormCollection() {
         MongoDatabase database = mongoDatabase();
-        return database.getCollection("mentorInterestForms");
+        return database.getCollection("applicationForms");
     }
     
-    @Bean
-    public MongoCollection<Document> menteeFormCollection() {
-        MongoDatabase database = mongoDatabase();
-        return database.getCollection("menteeInterestForms");
-    }
-    
+
 
 }
